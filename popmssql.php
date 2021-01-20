d9130fceb09adc548900f3fc594fa908c6323bf4
Sander <35436862+sndrmn@users.noreply.github.com>
2021-01-20 13:11:09 +1100

    commit

--- popmssql.php
+++ popmssql.php
@@ -50,7 +50,8 @@
               $db = $_SESSION['db'];
               $un = $_SESSION['un'];
               $pa = $_SESSION['pa'];
-
+              
+              $temp = substr($ep, -3);
               if ($temp == "com") {
                 $output = shell_exec("sqlcmd -S $ep -U $un -P $pa -Q 'CREATE DATABASE PROD'");
               }
